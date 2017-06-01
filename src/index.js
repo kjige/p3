660e2cc93463674cd816267c6eb7ffeae7adbdd5
kjige <chrisleongsc2@gmail.com>
2017-05-31 23:09:30 -0500

    1

--- src/index.js
+++ src/index.js
@@ -1,78 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ButtonToolbar, Button} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import './index.css';
 
-
-
-class Board extends React.Component {
+class Game extends React.Component {
+  
   constructor(){
     super();
     this.state = {
-      squares: Array(9).fill(null)
+      count: 0,
     }
   }
 
-  
-
-  handleClick(i){
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+  handleClick(){
+    this.setState({
+      count: this.state.count+1
+    });
   }
 
   render() {
-    const status = 'Next player: X';
 
     return (
       <div>
-        
+        <div className="row">       
+          <div className="col-xs-4 col-xs-offset-4 text-center">
+
+            <Button bsStyle="primary" onClick={this.handleClick.bind(this)}>Primary</Button>
+
+            <h1>{this.state.count}</h1>
+
+          </div>
+          <div className="col-xs-3 text-center">
+
+          </div>
+        </div> 
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-
-  render() {
-
-
-    return (
-      
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      
-        <ButtonToolbar>
-        {/* Standard button */}
-        <Button>Default</Button>
-
-        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-        <Button bsStyle="primary">Primary</Button>
-
-        {/* Indicates a successful or positive action */}
-        <Button bsStyle="success">Success</Button>
-
-        {/* Contextual button for informational alert messages */}
-        <Button bsStyle="info">Info</Button>
-
-        {/* Indicates caution should be taken with this action */}
-        <Button bsStyle="warning">Warning</Button>
-
-        {/* Indicates a dangerous or potentially negative action */}
-        <Button bsStyle="danger">Danger</Button>
-
-        {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-        <Button bsStyle="link">Link</Button>
-      </ButtonToolbar>
-
-      </div>
-      
     );
   }
 }
